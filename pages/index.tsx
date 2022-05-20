748ec3f9ee10cdd8b94baf80ec6d70a813329758
wuweiping <wuweipingface@gmail.com>
2022-05-20 20:13:14 +0800

    add global css

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
             <PostCard post={post} key={post.title} />
           ))}
         </div>
-
+        {/* what is the lg does */}
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
             <PostWidget />
