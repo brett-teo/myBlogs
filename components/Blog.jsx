dca905a30d506ad8309c01b6a9a011797a834b98
wuweiping <wuweipingface@gmail.com>
2022-05-20 20:06:58 +0800

    start draw the UI

--- components/Blog.jsx
+++
@@ -1,7 +0,0 @@
-const Blog = () => {
-  return (
-    <div className="p-20 resize-y shadow-2xl rounded-xl bg-gradient-to-l justify-center flex  ">Blog</div>
-  )
-}
-
-export default Blog
---
+++ components/index.js
@@ -0,0 +1,4 @@
+// TIP
+export { default as PostCard } from './PostCard'
+export { default as PostWidget } from './PostWidget'
+export { default as Categories } from './Categories'
